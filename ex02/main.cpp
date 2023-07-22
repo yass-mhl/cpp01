7358ff8c462f4bbfc60075afd5fb85f5531b9e20
Yassin MEHLIL <ymehlil@paul-f4Ar6s9.clusters.42paris.fr>
2023-07-22 11:55:47 +0200

    print ref, ptr and str address

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,20 +6,24 @@
 /*   By: ymehlil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 12:20:23 by ymehlil           #+#    #+#             */
-/*   Updated: 2023/07/19 12:27:33 by ymehlil          ###   ########.fr       */
+/*   Updated: 2023/07/22 11:55:03 by ymehlil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
 int main()
 {
 	std::string str = "HI THIS IS BRAIN";
-	
+
 	std::string *stringPTR = &str;
 	std::string& stringREF = str;
 
-	
+	std::cout << "str address : " << &str << std::endl;
+	std::cout << "stringPTR address : " << stringPTR << std::endl;
+	std::cout << "stringREF address : " << &stringREF << std::endl;
+
 	std::cout << *stringPTR << std::endl;
 	std::cout << stringREF << std::endl;
 }
